ada14a03dc0e630e8757b8a52bc3846d4b0d4a5f
PatilAntariksh <159646979+PatilAntariksh@users.noreply.github.com>
2025-04-21 21:45:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,41 @@ const io = socketIo(server, {
   cors: { origin: "*" }
 });
 
-io.on("connection", (socket) => {
-  socket.on("join", (room) => socket.join(room));
+let rooms = {}; // Track users per room
 
-  socket.on("offer", (data) => socket.to(data.room).emit("offer", data));
-  socket.on("answer", (data) => socket.to(data.room).emit("answer", data));
-  socket.on("candidate", (data) => socket.to(data.room).emit("candidate", data));
+io.on('connection', (socket) => {
+  socket.on('join', (room) => {
+    socket.join(room);
+
+    if (!rooms[room]) rooms[room] = [];
+    rooms[room].push(socket.id);
+
+    // First user creates the room
+    if (rooms[room].length === 1) {
+      socket.emit('room_created');
+    } else {
+      // Second user joins and tells the first user to start the call
+      socket.emit('room_joined');
+      socket.to(room).emit('ready'); // notify caller to send offer
+    }
+
+    socket.on('disconnect', () => {
+      rooms[room] = rooms[room].filter(id => id !== socket.id);
+      if (rooms[room].length === 0) delete rooms[room];
+    });
+  });
+
+  socket.on('offer', (data) => {
+    socket.to(data.room).emit('offer', data);
+  });
+
+  socket.on('answer', (data) => {
+    socket.to(data.room).emit('answer', data);
+  });
+
+  socket.on('candidate', (data) => {
+    socket.to(data.room).emit('candidate', data);
+  });
 });
 
 const PORT = process.env.PORT || 3000;
